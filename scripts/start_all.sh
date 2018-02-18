391f746d1d60492a675875a0dfb254d25025a2ee
Nathan Kern <nathan.kern@lehigh.edu>
2018-02-18 14:41:13 -0500

    Clean up output directory before starting new jobs

--- scripts/start_all.sh
+++ scripts/start_all.sh
@@ -8,13 +8,17 @@ if [ -e "$PIDS" ]; then
     exit 1
 fi
 
+rm $OUTPUT_DIR/*err
+rm $OUTPUT_DIR/*out
+
 # log into each host, then start the node
 awk '{print $1}' $ADDRESSES | while read hname; do
     echo "Starting $hname"
 # begin SSH template
 ssh -T -q $hname << TEMPLATE &
 cd $SCRIPT_DIR
-python $NODE_SCRIPT 2> $OUTPUT_DIR/\$(hostname).err
+source setup_env.sh
+python $NODE_SCRIPT 2> $OUTPUT_DIR/\$(hostname).err > $OUTPUT_DIR/\$(hostname).out
 echo "$! : \$(hostname)" >> $PIDS
 TEMPLATE
 # end SSH template
